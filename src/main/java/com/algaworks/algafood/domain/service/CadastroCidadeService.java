1032b2a84bf08f6cc0e387c29f24bc7eadcb765c
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2020-11-25 20:16:39 -0300

    backup

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCidadeService {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+}
