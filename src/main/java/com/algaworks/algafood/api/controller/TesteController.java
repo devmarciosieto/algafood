66158404cb716963dfe0fe53be38e9dd3646e137
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2020-11-26 14:59:49 -0300

    backup

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,13 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/teste")
+@RequestMapping("/testes")
 public class TesteController {
 
     @Autowired
@@ -15,5 +22,27 @@ public class TesteController {
 
     @Autowired
     private RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhasUnicaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/like")
+    public List<Cozinha> cozinhasLikePorNome(String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restauraurantesTaxaFrete(
+            BigDecimal taxaInicial, BigDecimal taxaFinal
+    ) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
 
 }
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -3,6 +3,11 @@ package com.algaworks.algafood.domain.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>{
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 }
